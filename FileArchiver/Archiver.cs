3ab6a8650c78311b715a91d617dde2c87c8bb8cf
gus-work <gus@gusperez.com>
2013-09-05 13:57:46 -0700

    Hydrating with old code...

---
+++ FileArchiver/Archiver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace FileArchiver
+{
+    class Archiver
+    {
+        private FileInfo sourceFile;
+        private DirectoryInfo targetDir;
+
+        public Archiver(string sourceFile, string targetDir)
+        {
+            this.sourceFile = new FileInfo(sourceFile);
+            if (!this.sourceFile.Exists)
+            {
+                throw new FileNotFoundException("Could not find " + sourceFile);
+            }
+
+            this.targetDir = new DirectoryInfo(targetDir);
+            if (!this.targetDir.Exists)
+            {
+                this.targetDir.Create();
+            }
+        }
+
+        public void Archive()
+        {
+            string targetFileName = Path.GetFileNameWithoutExtension(sourceFile.FullName) + 
+                "." + DateTime.Now.ToString("yyyyMMdd") + 
+                "." + DateTime.Now.ToString("HHmmss") + 
+                sourceFile.Extension;
+
+            sourceFile.CopyTo(targetDir.FullName + @"\" + targetFileName, true);
+        }
+    }
+}
---
+++ FileArchiver/Driver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace FileArchiver
+{
+    class Driver
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("A full path to the source file and target directory must be specified as command-line arguments.");
+                return 1;
+            }
+
+            Archiver archiver;
+
+            try
+            {
+                archiver = new Archiver(args[0], args[1]);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Either the source file does not exist or the target directory does not exist and could not be created.");
+                Console.WriteLine("More details:\n" + e.Message);
+                return 1;
+            }
+
+            try
+            {
+                archiver.Archive();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Failed to copy the source file to the target directory.");
+                Console.WriteLine("More details:\n" + e.Message);
+                return 2;
+            }
+
+            return 0;
+        }
+    }
+}
